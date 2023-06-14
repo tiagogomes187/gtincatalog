5cba1e3bb332cf9c857ee56eb9f7081ee86c6943
Tiago Gomes <tiagogomes187@gmail.com>
2023-06-13 21:54:49 -0300

    Backend project created

---
+++ backend/src/main/java/br/dev/tiagogomes/gtincatalog/GtincatalogApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.gtincatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtincatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtincatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/dev/tiagogomes/gtincatalog/GtincatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.gtincatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtincatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
