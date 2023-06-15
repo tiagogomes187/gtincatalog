be89093a88809a3226783766e9b3a0e58cea869d
Tiago Gomes <tiagogomes187@gmail.com>
2023-06-14 23:19:55 -0300

    Find Category by id, exception handling

--- backend/src/main/java/br/dev/tiagogomes/gtincatalog/services/CategoryService.java
+++ backend/src/main/java/br/dev/tiagogomes/gtincatalog/services/CategoryService.java
@@ -3,11 +3,14 @@ package br.dev.tiagogomes.gtincatalog.services;
 import br.dev.tiagogomes.gtincatalog.dto.CategoryDTO;
 import br.dev.tiagogomes.gtincatalog.entities.Category;
 import br.dev.tiagogomes.gtincatalog.repositories.CategoryRepository;
+import br.dev.tiagogomes.gtincatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,5 +24,11 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
-    
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(UUID id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
 }
