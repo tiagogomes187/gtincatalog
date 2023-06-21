abc44bdb57194635b9d35f3554e4ac3cc1bf80d4
Tiago Gomes <tiagogomes187@gmail.com>
2023-06-20 21:29:49 -0300

    Tests, TestFactory

---
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -0,0 +1,33 @@
+package tests.entities;
+
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
+
+public class AccountTests {
+
+    @Test
+    public void depositShouldIncreaseBalanceAndDiscontFeeWhenPositiveAmount(){
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account acc = AccountFactory.createEmptyAccount();
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount(){
+        double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+        double amount = -200.0;
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+}
