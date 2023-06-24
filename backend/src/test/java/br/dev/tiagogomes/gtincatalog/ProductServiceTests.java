1fab0c288de958884827ec6547ec5555b85de197
Tiago Gomes <tiagogomes187@gmail.com>
2023-06-23 21:16:06 -0300

    ProductServiceTests

---
+++ backend/src/test/java/br/dev/tiagogomes/gtincatalog/ProductServiceTests.java
@@ -0,0 +1,132 @@
+package br.dev.tiagogomes.gtincatalog;
+
+import br.dev.tiagogomes.gtincatalog.dto.ProductDTO;
+import br.dev.tiagogomes.gtincatalog.entities.Category;
+import br.dev.tiagogomes.gtincatalog.entities.Product;
+import br.dev.tiagogomes.gtincatalog.repositories.CategoryRepository;
+import br.dev.tiagogomes.gtincatalog.repositories.ProductRepository;
+import br.dev.tiagogomes.gtincatalog.services.ProductService;
+import br.dev.tiagogomes.gtincatalog.services.exceptions.DatabaseException;
+import br.dev.tiagogomes.gtincatalog.services.exceptions.ResourceNotFoundException;
+import br.dev.tiagogomes.gtincatalog.tests.Factory;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
+    private Category category;
+    private ProductDTO productDTO;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 2L;
+        dependentId = 3L;
+        product = Factory.createProduct();
+        productDTO = Factory.createProductDTO();
+        category = Factory.createCategory();
+        page = new PageImpl<>(List.of(product));
+        Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
+        Mockito.when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        Mockito.when(productRepository.getReferenceById(existingId)).thenReturn(product);
+        Mockito.when(productRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(categoryRepository.getReferenceById(existingId)).thenReturn(category);
+        Mockito.when(categoryRepository.getReferenceById(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(productRepository.save(ArgumentMatchers.any())).thenReturn(product);
+        Mockito.when(productRepository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+        Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
+        Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.update(nonExistingId,productDTO);
+        });
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() {
+        ProductDTO result = productService.update(existingId, productDTO);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.findById(nonExistingId);
+        });
+    }
+
+    @Test
+    public void findByIdShouldReturnProductDTOWhenIdExists() {
+        ProductDTO result = productService.findById(existingId);
+        Assertions.assertNotNull(result);
+    }
+
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ProductDTO> result = productService.findAllPaged(pageable);
+        Assertions.assertNotNull(result);
+        Mockito.verify(productRepository, Mockito.times(1)).findAll(pageable);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            productService.delete(dependentId);
+        });
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.delete(nonExistingId);
+        });
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(() -> {
+            productService.delete(existingId);
+        });
+    }
+}
--- backend/src/test/java/br/dev/tiagogomes/gtincatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/br/dev/tiagogomes/gtincatalog/repositories/ProductRepositoryTests.java
@@ -1,11 +1,12 @@
 package br.dev.tiagogomes.gtincatalog.repositories;
 
 import br.dev.tiagogomes.gtincatalog.entities.Product;
+import br.dev.tiagogomes.gtincatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -15,11 +16,44 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
 
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProduct;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProduct = 25L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
         productRepository.deleteById(existingId);
         Optional<Product> result = productRepository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull () {
+        Product product = Factory.createProduct();
+        product.setId(null);
+        product = productRepository.save(product);
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProduct + 1, product.getId());
+
+    }
+
+    @Test
+    public void findByIdShouldReturnNonEmptyOptionalWhenIdExists () {
+        Optional<Product> result = productRepository.findById(existingId);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void finddByIdShouldReturnEmptyOptionalWhenIdDoesNotExists () {
+        Optional<Product> result = productRepository.findById(nonExistingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
 }
