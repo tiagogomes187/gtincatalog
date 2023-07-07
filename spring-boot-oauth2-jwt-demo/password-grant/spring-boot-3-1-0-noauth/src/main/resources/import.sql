3be1f320f50bb2448b6ef6a0a15c224a422d0e32
Tiago Gomes <tiagogomes187@gmail.com>
2023-07-06 21:55:14 -0300

    BCrypt password

--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,12 +1,14 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$tjaNxXSYRbBiAVeljkpRJ.gxo/mAsLEtA0Ah7OPyuEvLxfMEqwUdW');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$tjaNxXSYRbBiAVeljkpRJ.gxo/mAsLEtA0Ah7OPyuEvLxfMEqwUdW');
+INSERT INTO tb_user (name, email, password) VALUES ('Tiago', 'tiago@gmail.com', '$2a$10$mibNnYT1.eldqYST4inyuegRHIlFYyCC0cN6rXCN5urtnzm4Bxs6G');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1,1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2,1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2,2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3,1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3,2);
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
