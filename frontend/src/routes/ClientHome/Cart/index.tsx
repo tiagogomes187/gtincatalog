7f9f8aa528c58b69ab374c381061e7ffcc4c3fa1
Tiago Gomes <tiagogomes187@gmail.com>
2023-11-01 20:11:15 -0300

    Increase/Decrease cart items

--- frontend/src/routes/ClientHome/Cart/index.tsx
+++ frontend/src/routes/ClientHome/Cart/index.tsx
@@ -12,6 +12,17 @@ export default function Cart() {
     setCart(cartService.getCart());
   }
 
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
+
+  function handleDecreaseItem(productId: number) {
+    cartService.decreaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -35,9 +46,14 @@ export default function Cart() {
                       {item.reference} - {item.name} - {item.color}
                     </h3>
                     <div className="dsc-cart-item-quantity-container">
-                      <div className="dsc-cart-item-quantity-btn">-</div>
+                      <div onClick={() => handleDecreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">-</div>
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div
+                        onClick={() => handleIncreaseItem(item.productId)}
+                        className="dsc-cart-item-quantity-btn"
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -58,7 +74,9 @@ export default function Cart() {
           <Link to={"/catalog"}>
             <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
           </Link>
-          <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">Limpar Carrinho</div>
+          <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">
+            Limpar Carrinho
+          </div>
         </div>
       </section>
     </main>
