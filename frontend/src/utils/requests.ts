f4d803112fe66ec314fb1e3e508ae76b613fd23a
Tiago Gomes <tiagogomes187@gmail.com>
2023-11-06 23:16:54 -0300

    requestBackend update withCredentials

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,6 +1,14 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
+import * as authService from "../services/auth-service";
 
-export function requestBackend (config: AxiosRequestConfig) {
-    return axios({...config, baseURL: BASE_URL});
+export function requestBackend(config: AxiosRequestConfig) {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: "Bearer " + authService.getAccessToken(),
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: BASE_URL, headers });
 }
