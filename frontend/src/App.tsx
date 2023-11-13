148b6d251742c5efedc0782286fb61e16cdeb1c1
Tiago Gomes <tiagogomes187@gmail.com>
2023-11-13 20:40:44 -0300

    Access control route level

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ export default function App() {
           <Route
             path="/admin/"
             element={
-              <PrivateRoute>
+              <PrivateRoute roles={['ROLE_ADMIN']}>
                 <Admin />
               </PrivateRoute>
             }
