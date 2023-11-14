16d4aa4a429ca34640231fd35185093009d9dff8
Tiago Gomes <tiagogomes187@gmail.com>
2023-11-13 21:19:18 -0300

    Context token payload

--- frontend/src/routes/ClientHome/Login/index.tsx
+++ frontend/src/routes/ClientHome/Login/index.tsx
@@ -1,9 +1,15 @@
 import "./styles.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { CredentialsDTO } from "../../../models/auth";
 import * as authService from "../../../services/auth-service";
+import { useNavigate } from "react-router-dom";
+import { ContextToken } from "../../../utils/context-token";
 
 export default function Login() {
+  const { setContextTokenPayload } = useContext(ContextToken);
+
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState<CredentialsDTO>({
     username: "",
     password: "",
@@ -16,6 +22,8 @@ export default function Login() {
       .loginRequest(formData)
       .then((response) => {
         authService.saveAccessToken(response.data.access_token);
+        setContextTokenPayload(authService.getAccessTokenPayload());
+        navigate("/cart");
       })
       .catch((error) => {
         console.log("Erro no login ", error);
