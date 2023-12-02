d7034c3bd5e03af1e9fd468313399379616943c6
Tiago Gomes <tiagogomes187@gmail.com>
2023-12-02 01:48:37 -0300

    Ajustei as variáveis do arquivo system.ts

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,9 +1,8 @@
-export const BASE_URL = "http://localhost:8080";
-
 export const CART_KEY = "br.dev.tiagogomes.dscommerce/Cart";
-
 export const TOKEN_KEY = "br.dev.tiagogomes.dscommerce/Token";
 
-export const CLIENT_ID = "myclientid";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 
-export const CLIENT_SECRET = "myclientsecret";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
